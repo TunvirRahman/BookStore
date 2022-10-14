5bb7a8f5f8b4fc42964a1583a627a48dcf25dd33
Tunvir Rahman <tunvir.rahman07@gmail.com>
2022-10-14 20:48:22 +0600

    Continue

--- app/src/main/java/com/example/bookstore/MainActivity.kt
+++ app/src/main/java/com/example/bookstore/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.example.bookstore
 
+import android.graphics.drawable.shapes.RoundRectShape
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.bookstore.ui.theme.BookStoreTheme
 
@@ -30,14 +35,12 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+
+fun Greeting(text:String){
+    return Text(text = "Hello $text",textAlign = TextAlign.Center)
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    BookStoreTheme {
-        Greeting("Android")
-    }
-}
+//@Composable
+//fun Greeting(name: String) {
+//    Text(text = "Hello $name!")
+//}
