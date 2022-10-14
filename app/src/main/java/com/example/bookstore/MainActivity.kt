13516816d144edda021277c7edb4900e143539bc
Tunvir Rahman <tunvir.rahman07@gmail.com>
2022-10-14 22:09:47 +0600

    Continue

--- app/src/main/java/com/example/bookstore/MainActivity.kt
+++ app/src/main/java/com/example/bookstore/MainActivity.kt
@@ -1,18 +1,24 @@
 package com.example.bookstore
 
 import android.graphics.drawable.shapes.RoundRectShape
+import android.graphics.drawable.shapes.Shape
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.bookstore.ui.theme.BookStoreTheme
@@ -24,20 +30,21 @@ class MainActivity : ComponentActivity() {
             BookStoreTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    modifier = Modifier.fillMaxSize()
                 ) {
-                    Greeting("Android")
+                    Column() {
+
+                        Greeting(title = "Android")
+                        Greeting(title = "React")
+                    }
                 }
             }
         }
     }
 }
-
 @Composable
-
-fun Greeting(text:String){
-    return Text(text = "Hello $text",textAlign = TextAlign.Center)
+fun Greeting(title:String){
+    return Text(text = "Welcome to $title", textAlign = TextAlign.Center, fontWeight = FontWeight.Bold)
 }
 
 //@Composable
